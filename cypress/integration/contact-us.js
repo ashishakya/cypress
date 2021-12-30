c7b05a89ef3414cd626b2591925b9f922bfdf4e1
Ashish Shakya <ashishakya@outlook.com>
2021-12-30 23:28:37 +0545

    cross verify the test

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -5,19 +5,27 @@ const assert = require("assert");
 
 describe("Test contact us page form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
+        //prepare
         cy.visit(landingPageUrl);
+
+        //perform
         cy.get('[name="first_name"]').type("first name");
         cy.get('[name="last_name"]').type("last name");
         cy.get('[name="email"]').type("testemail@email.com");
         cy.get('textarea.feedback-input').type("this is my demo feedback");
         cy.get('[type="submit"]').click()
-        // console.log(cy.get('#contact_reply').find("h1").text());
-        // const successMessage = "Thank You for your Message!"
-        // assert.strictEqual(cy.get('h1'), successMessage);
+
+        // predict: cross verify: assertion
+        const successMessage = "Thank You for your Message!";
+        cy.get('#contact_reply > h1').should("have.text", successMessage)
     });
 
     it("Should not be able to submit a successful submission via contact us form as all field are required", () => {
         cy.visit(landingPageUrl);
+
         cy.get('[type="submit"]').click()
+
+        // cross verify
+        cy.get('body').should("not.be.empty");
     });
 })
