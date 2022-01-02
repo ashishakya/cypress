72a42c5719d42d2efe0de30d549f7e978b9b32c9
Ashish Shakya <ashishakya@outlook.com>
2022-01-02 17:01:08 +0545

    comand chaining and better inspection

---
+++ cypress/integration/automation-test-store/inspect-item.js
@@ -0,0 +1,40 @@
+/// <reference types="Cypress" />
+
+const landingPageUrl = "https://automationteststore.com/";
+const assert = require("assert");
+
+describe("Inspect automation Test Store item using chain of commands", () => {
+    it("Click on the first item using item header", () => {
+        //prepare
+        cy.visit(landingPageUrl);
+
+        // perform
+        cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click()
+
+
+        //predict
+
+    });
+
+    it("Click on the first item using item text", () => {
+        //prepare
+        cy.visit(landingPageUrl);
+
+        // perform
+        cy.get('.prdocutname').contains("Skinsheen Bronzer Stick").click();
+
+        //predict
+
+    });
+
+    it("Click on the first item using item using index", () => {
+        //prepare
+        cy.visit(landingPageUrl);
+
+        // perform
+        cy.get('.fixed_wrapper').find(".prdocutname").eq(0).click();
+
+        //predict
+
+    });
+})
