e58ed928f66f47ff462174cbcde2327a34686f41
Ashish Shakya <ashishakya@outlook.com>
2022-01-04 17:18:37 +0545

    alias, loop and logic

--- cypress/integration/automation-test-store/alias-invoke.js
+++ cypress/integration/automation-test-store/alias-invoke.js
@@ -15,7 +15,7 @@ describe("Alias and invoke", () => {
 
     })
 
-    it.only("Count the number of items in homepage", ()=>{
+    it("Count the number of items in homepage", ()=>{
         cy.visit(landingPageUrl);
         cy.get(".thumbnails .thumbnail").as("itemsElementList")
 
@@ -26,7 +26,7 @@ describe("Alias and invoke", () => {
         });
     })
 
-    it.only("Validating product count and label", ()=>{
+    it("Validating product count and label", ()=>{
         cy.visit(landingPageUrl);
         cy.get(".thumbnails .thumbnail").as("itemsElementList")
 
@@ -35,5 +35,45 @@ describe("Alias and invoke", () => {
         cy.get("@itemsElementList").find(".productcart").each((el)=>{
             cy.wrap(el).should("have.attr", "title", "Add to Cart")
         });
+    })
+
+    it.only("Calculate total of normal and sale product", ()=>{
+        cy.visit(landingPageUrl);
+        cy.get(".thumbnails .thumbnail").as("itemsElementList")
+
+
+        // count of non sale item
+        // cy.get("@itemsElementList").find(".price .oneprice").each((el)=>{
+        //     cy.log(el.text())
+        // })
+
+        cy.get("@itemsElementList").find(".price .oneprice").invoke('text').as("itemPriceList")
+        cy.get("@itemsElementList").find(".price .pricenew").invoke('text').as("saleItemPriceList")
+
+        let totalPrice = 0;
+
+        cy.get("@itemPriceList").then((itemPrice)=>{
+            let normalItemTotal = 0;
+            const itemPrices = itemPrice.split("$")
+            itemPrices.forEach(itemPrice=>{
+                normalItemTotal+=Number(itemPrice)
+            })
+            totalPrice+=normalItemTotal
+            cy.log("sale item total price-->>", normalItemTotal)
+        })
+
+        cy.get("@saleItemPriceList").then((itemPrice)=>{
+            let saleItemTotal = 0;
+            const itemPrices = itemPrice.split("$")
+            itemPrices.forEach(itemPrice=>{
+                saleItemTotal+=Number(itemPrice)
+            })
+            totalPrice+=saleItemTotal
+
+            cy.log("sale item total price-->>", saleItemTotal)
+        }).then(()=>{
+            cy.log('total price->>', totalPrice)
+            expect(totalPrice).to.eq(648.5)
+        })
     })
 })
