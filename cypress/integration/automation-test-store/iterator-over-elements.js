0cfe09c22d4ffad450a9a3fbd4f3761171a1dcbb
Ashish Shakya <ashishakya@outlook.com>
2022-01-04 09:45:32 +0545

    iterator

---
+++ cypress/integration/automation-test-store/iterator-over-elements.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+
+const landingPageUrl = "https://automationteststore.com/";
+const assert = require("assert");
+
+describe("Iterate over elements", () => {
+    it("Log info of all hair care products", ()=>{
+        cy.visit(landingPageUrl);
+        const menuTitle = "Hair Care";
+        cy.get("a[href*='product/category&path='").contains(menuTitle).click()
+        cy.get(".fixed_wrapper .prdocutname").each((el)=>{
+            cy.log(el.text());
+        })
+
+    })
+
+    it("Add specific product to basket", ()=>{
+        cy.visit(landingPageUrl);
+        const menuTitle = "Hair Care";
+        cy.get("a[href*='product/category&path='").contains(menuTitle).click()
+        cy.get(".fixed_wrapper .prdocutname").each((el)=>{
+            if(el.text().includes("Seaweed Conditioner")){
+                cy.wrap(el).click() // el is an jquery instance hence wrap helps us to wrap it as a cypres instance
+            }
+        })
+    })
+})
