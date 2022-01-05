5bf575a765eb584f1e28b54301a118bef823493f
Ashish Shakya <ashishakya@outlook.com>
2022-01-05 17:53:51 +0545

    basic of stubs

--- cypress/integration/webdriver-uni/events.js
+++ cypress/integration/webdriver-uni/events.js
@@ -6,30 +6,42 @@ describe("Handle js alert", () => {
     it("Confirm js alert contains the correct text", () => {
         //prepare
         cy.visit(landingPageUrl);
-        cy.get("#popup-alerts").invoke("removeAttr", "target").click({force:true})
+        cy.get("#popup-alerts").invoke("removeAttr", "target").click({force: true})
         cy.get("#button1").click()
-        cy.on("window:alert", (alertMessage)=>{
+        cy.on("window:alert", (alertMessage) => {
             expect(alertMessage).to.equal("I am an alert box!")
         })
     });
 
-    it("Validate js Confirm actions on confirm modal", ()=>{
+    it("Validate js Confirm actions on confirm modal", () => {
         cy.visit(landingPageUrl);
-        cy.get("#popup-alerts").invoke("removeAttr", "target").click({force:true})
+        cy.get("#popup-alerts").invoke("removeAttr", "target").click({force: true})
         cy.get("#button4").click()
-        cy.on("window:confirm", ()=>{
-           return true
+        cy.on("window:confirm", () => {
+            return true
         })
         cy.get("#confirm-alert-text").contains("You pressed OK!")
     })
 
-    it("Validate js cancel actions on confirm modal", ()=>{
+    it("Validate js cancel actions on confirm modal", () => {
         cy.visit(landingPageUrl);
-        cy.get("#popup-alerts").invoke("removeAttr", "target").click({force:true})
+        cy.get("#popup-alerts").invoke("removeAttr", "target").click({force: true})
         cy.get("#button4").click()
-        cy.on("window:confirm", ()=>{
+        cy.on("window:confirm", () => {
             return false
         })
         cy.get("#confirm-alert-text").contains("You pressed Cancel!")
+    })
+
+    it.only("Validate js confirm alert box using a stub", () => {
+        cy.visit(landingPageUrl);
+        cy.get("#popup-alerts").invoke("removeAttr", "target").click({force: true})
+        const stubs = cy.stub();
+        cy.on("window:confirm", stubs)
+        cy.get("#button4").click().then(() => {
+            expect(stubs.getCall(0)).to.be.calledWith("Press a button!")
+        })
+            .then(() => true)
+            .then(() =>cy.get("#confirm-alert-text").contains("You pressed OK!"))
     })
 })
