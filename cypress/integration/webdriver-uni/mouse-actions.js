5c6ec8ba87db0bce730a7db16bcf46b4946ebb78
Ashish Shakya <ashishakya@outlook.com>
2022-01-10 14:04:36 +0545

    double click

--- cypress/integration/webdriver-uni/mouse-actions.js
+++ cypress/integration/webdriver-uni/mouse-actions.js
@@ -16,4 +16,23 @@ describe("Verify mouse actions", ()=>{
 
         cy.get('#droppable').trigger("mousemove").trigger("mouseup", {force:true})
     });
+
+    it('Verify double mouse click', function () {
+        cy.visit(landingPageUrl);
+
+        cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click({force:true})
+
+        cy.get('#double-click').trigger("dblclick")
+    });
+
+    it.only('Verify mouse click and hold', function () {
+        cy.visit(landingPageUrl);
+
+        cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click({force:true})
+
+        cy.get('#click-box').trigger("mousedown")
+            .then((element)=>{
+                expect(element).to.have.css("background-color", "rgb(0, 255, 0)")
+        })
+    });
 })
