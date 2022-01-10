2809f908776ec6fc5faa218679186f729952bed6
Ashish Shakya <ashishakya@outlook.com>
2022-01-10 09:52:26 +0545

    Autocomplete

---
+++ cypress/integration/webdriver-uni/autocomplete.js
@@ -0,0 +1,22 @@
+const landingPageUrl = "http://webdriveruniversity.com";
+
+describe("Verify state of autocomplete", ()=>{
+    it('Select specific product via auto complete', function () {
+        cy.visit(landingPageUrl);
+
+        cy.get("#autocomplete-textfield").invoke("removeAttr", "target").click({force: true})
+        cy.get('#myInput').type("g")
+        cy.get("#myInputautocomplete-list input").as("items")
+
+        cy.get("@items").each((item, index)=>{
+            const productToSelect = "Garlic";
+            if(item.attr("value")===productToSelect){
+                item.click();
+                cy.get('#submit-button').click()
+                cy.log(cy.url());
+                cy.url().should("include", productToSelect);
+            }
+        })
+
+    });
+})
