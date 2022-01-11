955923d9a0a5f6c5d9d8220821fc95456b53f552
Ashish Shakya <ashishakya@outlook.com>
2022-01-11 11:15:04 +0545

    handling data

---
+++ cypress/integration/webdriver-uni/data-tables.js
@@ -0,0 +1,26 @@
+const landingPageUrl = "http://webdriveruniversity.com";
+
+describe("Handling data via web driver uni", () => {
+    beforeEach(() => {
+        cy.visit(landingPageUrl);
+
+        cy.get("#data-table").invoke("removeAttr", "target").click({force: true})
+    })
+
+    it('Verify the total of age field of all users', function () {
+        const userDetails = [];
+        let totalAge = 0;
+
+        cy.get("#thumbnail-1 td").each((el, index) => {
+            userDetails[index] = el.text();
+        }).then(() => {
+                var  i;
+                for(i=0; i< userDetails.length; i++){
+                    if(Number(userDetails[i])){
+                        totalAge+=Number(userDetails[i])
+                    }
+                }
+                expect(totalAge).equals(322)
+        })
+    })
+})
