032707599aa1239a4624fb1185b625ba53a8dfd9
Ashish Shakya <ashishakya@outlook.com>
2022-01-17 22:12:19 +0545

    refactor logic using before hooks in radio.js

--- cypress/integration/webdriver-uni/radio.js
+++ cypress/integration/webdriver-uni/radio.js
@@ -1,20 +1,17 @@
 const landingPageUrl = "http://webdriveruniversity.com";
 
 describe("Verify radio button", ()=>{
-    it('check and validation checkbox ', function () {
+    before(()=>{
         cy.visit(landingPageUrl);
-
         cy.get("#dropdown-checkboxes-radiobuttons").invoke("removeAttr", "target").click({force: true})
+    })
 
+    it('check and validation checkbox ', function () {
         cy.get("#radio-buttons [type='radio']").first().check()
         cy.get("#radio-buttons [type='radio']").eq(2).check()
     });
 
-    it.only('verify selected radio option ', function () {
-        cy.visit(landingPageUrl);
-
-        cy.get("#dropdown-checkboxes-radiobuttons").invoke("removeAttr", "target").click({force: true})
-
+    it('verify selected radio option ', function () {
         cy.get('[value="lettuce"]').should("not.be.checked")
         cy.get('[value="pumpkin"]').should("be.checked")
         cy.get('[value="cabbage"]').should("be.disabled")
