d4d90472070cacd3c2d2d737d9160caad463f1a1
Ashish Shakya <ashishakya@outlook.com>
2022-01-22 16:29:53 +0545

    use of debugger

--- cypress/support/pageObjects/automationTestStore/hairCare_Po.js
+++ cypress/support/pageObjects/automationTestStore/hairCare_Po.js
@@ -1,7 +1,10 @@
-class HairCare_Po{
-        addHairCareProductsToBasket(){
-            // globalThis.data.productNames.forEach(product=>cy.addProductToBasket(product))
-
-        }
+class HairCare_Po {
+    addHairCareProductsToBasket() {
+        globalThis.data.productNames.forEach(product => cy.addProductToBasket(product).then(() => {
+            // debugger
+        }))
+        cy.get(".dropdown-toggle > .fa").click()
+    }
 }
+
 export default HairCare_Po
