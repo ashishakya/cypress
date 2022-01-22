b44b0d499f6efc3e06859614fe8a8d01ad8ea8d7
Ashish Shakya <ashishakya@outlook.com>
2022-01-22 16:11:25 +0545

    handling timeout

--- cypress/support/pageObjects/webDriverUni/homepage_PO.js
+++ cypress/support/pageObjects/webDriverUni/homepage_PO.js
@@ -1,6 +1,8 @@
 class Homepage_PO {
     visitHomePage(){
-        cy.visit(Cypress.env("web_driver_home_page_url"));
+        cy.visit(Cypress.env("web_driver_home_page_url"), {
+            timeout: 60000 // 60 secs
+        });
     }
     clickOnContactUsButton(){
         cy.get("#contact-us").invoke("removeAttr", "target").click({force: true});
